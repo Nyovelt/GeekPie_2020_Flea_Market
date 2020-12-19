b4a2609f8d53e41cfb5d020579227f4973371a09
Alex Qin <canarypwn@AlexdeMacBook-Pro.local>
2020-12-19 23:52:36 +0800

    init commit

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -28,11 +28,11 @@
       <v-spacer></v-spacer>
 
       <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        href="https://geekpie.club"
         target="_blank"
         text
       >
-        <span class="mr-2">Latest Release</span>
+        <span class="mr-2">About GeekPie_</span>
         <v-icon>mdi-open-in-new</v-icon>
       </v-btn>
     </v-app-bar>
--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -11,80 +11,97 @@
       </v-col>
 
       <v-col class="mb-4">
-        <h1 class="display-2 font-weight-bold mb-3">
-          Welcome to Vuetify
-        </h1>
-
-        <p class="subheading font-weight-regular">
-          For help and collaboration with other Vuetify developers,
-          <br>please join our online
-          <a
-            href="https://community.vuetifyjs.com"
-            target="_blank"
-          >Discord Community</a>
-        </p>
-      </v-col>
-
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-3">
-          What's next?
-        </h2>
+        <h1 class="display-2 font-weight-bold mb-3">这里是标题</h1>
 
         <v-row justify="center">
-          <a
-            v-for="(next, i) in whatsNext"
-            :key="i"
-            :href="next.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ next.text }}
-          </a>
+          <div class="text-center">
+            <v-btn
+              class="ma-2"
+              :loading="loading"
+              :disabled="loading"
+              color="secondary"
+              @click="loader = 'loading'"
+            >
+              按钮1
+            </v-btn>
+
+            <v-btn
+              :loading="loading3"
+              :disabled="loading3"
+              color="blue-grey"
+              class="ma-2 white--text"
+              @click="loader = 'loading3'"
+            >
+              按钮2
+            </v-btn>
+
+            <v-btn
+              class="ma-2"
+              :loading="loading2"
+              :disabled="loading2"
+              color="success"
+              @click="loader = 'loading2'"
+            >
+              按钮3
+            </v-btn>
+
+            <v-btn
+              class="ma-2"
+              :loading="loading4"
+              :disabled="loading4"
+              color="info"
+              @click="loader = 'loading4'"
+            >
+              按钮4
+            </v-btn>
+          </div>
+        </v-row>
+
+                <v-row justify="center">
+          <div class="text-center">
+            <v-btn
+              class="ma-2"
+              :loading="loading"
+              :disabled="loading"
+              color="cyan"
+              @click="loader = 'loading'"
+            >
+              按钮5
+            </v-btn>
+
+            <v-btn
+              :loading="loading3"
+              :disabled="loading3"
+              color="teal"
+              class="ma-2 white--text"
+              @click="loader = 'loading3'"
+            >
+              按钮6
+            </v-btn>
+
+            <v-btn
+              class="ma-2"
+              :loading="loading2"
+              :disabled="loading2"
+              color="primary"
+              @click="loader = 'loading2'"
+            >
+              按钮7
+            </v-btn>
+
+
+
+          </div>
         </v-row>
       </v-col>
 
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-3">
-          Important Links
-        </h2>
+      <v-col class="mb-5" cols="12">
+        <h2 class="headline font-weight-bold mb-3">这里是二级标题</h2>
 
         <v-row justify="center">
-          <a
-            v-for="(link, i) in importantLinks"
-            :key="i"
-            :href="link.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ link.text }}
-          </a>
-        </v-row>
-      </v-col>
-
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-3">
-          Ecosystem
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(eco, i) in ecosystem"
-            :key="i"
-            :href="eco.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ eco.text }}
-          </a>
+            
+            <v-btn depressed color="pink"> Continue </v-btn>
+          
         </v-row>
       </v-col>
     </v-row>
@@ -92,60 +109,9 @@
 </template>
 
 <script>
-  export default {
-    name: 'HelloWorld',
+export default {
+  name: "HelloWorld",
 
-    data: () => ({
-      ecosystem: [
-        {
-          text: 'vuetify-loader',
-          href: 'https://github.com/vuetifyjs/vuetify-loader',
-        },
-        {
-          text: 'github',
-          href: 'https://github.com/vuetifyjs/vuetify',
-        },
-        {
-          text: 'awesome-vuetify',
-          href: 'https://github.com/vuetifyjs/awesome-vuetify',
-        },
-      ],
-      importantLinks: [
-        {
-          text: 'Documentation',
-          href: 'https://vuetifyjs.com',
-        },
-        {
-          text: 'Chat',
-          href: 'https://community.vuetifyjs.com',
-        },
-        {
-          text: 'Made with Vuetify',
-          href: 'https://madewithvuejs.com/vuetify',
-        },
-        {
-          text: 'Twitter',
-          href: 'https://twitter.com/vuetifyjs',
-        },
-        {
-          text: 'Articles',
-          href: 'https://medium.com/vuetify',
-        },
-      ],
-      whatsNext: [
-        {
-          text: 'Explore components',
-          href: 'https://vuetifyjs.com/components/api-explorer',
-        },
-        {
-          text: 'Select a layout',
-          href: 'https://vuetifyjs.com/getting-started/pre-made-layouts',
-        },
-        {
-          text: 'Frequently Asked Questions',
-          href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions',
-        },
-      ],
-    }),
-  }
+  data: () => ({}),
+};
 </script>
